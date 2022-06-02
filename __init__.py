11ae7c7183c77d9d811de968c85b1f794bbc1f96
Brian Keriri <36098209+OdinXVI@users.noreply.github.com>
2022-06-02 05:19:14 -0500

    first commit

---
+++ __init__.py
@@ -0,0 +1,10 @@
+bl_info = {
+    "name": "Neural Rendering",
+    "version": (0, 1, 0),
+    "author": "Nelta",
+    "blender": (2, 90, 0),
+    "description": "Model and reconstruct radiance fields using TensoRF",
+    "location": "3D Viewport",
+    "doc_url": "",
+    "category": "",
+}
